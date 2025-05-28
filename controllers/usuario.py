2368ff8423cb2e6546f696ca17ab23508ea59e69
anamez22 <ana.amezquita@correounivalle.edu.co>
2025-05-28 16:00:25 -0500

    Se cargan la clase usuario en los controladores del proyecto

--- controllers/usuario.py
+++ controllers/usuario.py
@@ -1,29 +1,35 @@
+from Models.conexionBD import ConexionBD
 from tkinter import messagebox
-from models.conexionBD import ConexionDB
 
 
-class Usuario():
+class usuario():
     def __init__(self, nombreUsuario, password):
         self.nombreUsuario = nombreUsuario
         self.password = password
-        self.root = ""
-
-    def iniciarSesion(self, nombreUsuario, password):
-        miConexion = ConexionDB()
-        miConexion.crearConexion()
-        conn = miConexion.getConnetion()
-        cursor = conn.cursor()
-        cursor.execute("select * from usuario")
-        listaUsuario = cursor.fetchall()
-        for usuario in listaUsuario:
-            if (usuario[1] == nombreUsuario and usuario[2] == password):
+        self.rol = "" #no se sabe que rol tiene el usuario al loggearse se sabe si es admin o no
+    
+    def iniciarSesion(self,nombreUsuario,password):
+        miconexion= ConexionBD()
+        miconexion.crearConexion()
+        conn = miconexion.getConnection()
+        cursor = conn.cursor() #se encarga de ejecutar las consultas
+        cursor.execute("select * from usuario ")
+        listaUsuarios= cursor.fetchall() #fetchall todos los registros de la consulta
+        for  usuario in listaUsuarios:
+            if usuario[1] ==nombreUsuario and usuario[2] ==password:
                 self.rol = usuario[3]
-                if(self.rol == "admin"):
-                    messagebox.showinfo("Información", "Acceso correcto Administrador!")
-                    #crear objeto administrador y abrir menú Administrador
+                if self.rol == "admin":
+                    messagebox.showinfo("Informacion","Acceso correcto a administrador")
+                    #Crear objeto Administrador y abrir menu administrador y se pone else pq son 2 roles
                 else:
-                    messagebox.showinfo("Información", "Acceso correcto Digitador!")
-                miConexion.cerrarConexion()
+                    messagebox.showinfo("Informacion","Acceso correcto Digitador")
+                miconexion.cerrarConexion()
                 return 
-        messagebox.showwarning("Advertencia", "El nombre de usuario y/o contraseña no existe, verifique e intente nuevamente!")
+        messagebox.showwarning("ADVERTENCIA","El nombre de usuario y/o contraseña no existen, ¡verifique e intente nuevamente!")
 
+
+                
+
+
+
+    
